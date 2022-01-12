db72362deac5891b417e6add61b3d9881f81b3ad
ehis0075 <ehisjude420@gmail.com>
2022-01-12 12:11:55 +0100

    first

--- test/patientintake/DateTimeConverterShould.java
+++ test/patientintake/DateTimeConverterShould.java
@@ -16,12 +16,14 @@ class DateTimeConverterShould {
    @Nested
    @DisplayName("convert string with 'today' keyword")
    class TodayTests {
+
+
       @Test
       @DisplayName("correctly")
       void convertTodayStringCorrectly() {
-         LocalDate today = LocalDate.of(2018, 9, 1);
+         LocalDate today = LocalDate.of(2018, 9, 1);  //date time
          LocalDateTime result = DateTimeConverter.convertStringToDateTime("today 1:00 pm",
-            today);
+            today); //string
          assertEquals(result, LocalDateTime.of(2018, 9, 1, 13, 0),
             () -> "Failed to convert 'today' string to expected date time, today passed was: " +today);
       }
@@ -51,6 +53,7 @@ class DateTimeConverterShould {
       Throwable error = assertThrows(RuntimeException.class, () ->
          DateTimeConverter.convertStringToDateTime("9/2/2018 100 pm",
          LocalDate.of(2018, 9, 1)));
+
       assertEquals("Unable to create date time from: [9/2/2018 100 pm], " +
          "please enter with format [M/d/yyyy h:mm a], Text '9/2/2018 100 PM' " +
          "could not be parsed at index 12", error.getMessage());
